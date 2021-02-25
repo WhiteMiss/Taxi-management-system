f3f7549849590de79bb5f95f683fe6bbea24c2dd
xiehao <1448335038@qq.com>
2021-02-26 00:17:44 +0800

    2021 2 26
    积分兑换

--- vue-xuAdmin/src/views/integral/exchange.vue
+++ vue-xuAdmin/src/views/integral/exchange.vue
@@ -16,12 +16,24 @@
     <el-col :span="12">
       <div class="card bbxx">
         <p class="title"><i class="fa fa-server"></i>账户信息</p>
-
+<dv-scroll-ranking-board :config="config" style="width:500px;height:300px" />
       </div>
-
     </el-col>
   </el-row>
-<dv-scroll-ranking-board :config="config" style="width:500px;height:300px" />
+<el-row>
+  <el-col :span="6" v-for="(o, index) in 3" :key="o" :offset="index > 0 ? 3 : 0">
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <span>好吃的汉堡</span>
+        <div class="bottom clearfix">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">兑换</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+</el-row>
   </div>
 </template>
 <script>
@@ -29,6 +41,7 @@ export default {
   name: "exchange",
   data () {
     return {
+      currentDate: new Date(),
       "config": {
         "data": [
           {
@@ -42,15 +55,8 @@ export default {
           {
             name: "西峡",
             value: 78932
-          },
-          {
-            name: "驻马店",
-            value: 63411
-          },
-          {
-            name: "新乡",
-            value: 44231
           }
+
         ],
         unit: "单位",
         valueFormatter ({ value }) {
@@ -77,4 +83,34 @@ padding-left: 30px;
 padding-top: 15px;
 
 }
+/* 卡片 */
+ .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+
+  .clearfix:after {
+      clear: both
+  }
 </style>
